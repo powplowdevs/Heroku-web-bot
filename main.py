e57e8a176730115c08af53869b76c9d5f456fdee
powplowdevs <kalidfawzia@gmail.com>
2022-10-18 19:42:51 -0400

    update to main.py
    trying to get selenuim to show on site

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
 import os
-import sys
 
-driver = webdriver.Chrome("G:\My Drive\Programing\Personal scripts\Web\python selunium web bot\chromedriver.exe")
+chrome_options = webdriver.Chrome("G:\My Drive\Programing\Personal scripts\Web\python selunium web bot\chromedriver.exe")
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 input()
