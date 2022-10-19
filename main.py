62c91e147a29674c5145bf59c2423243864bccda
powplowdevs <kalidfawzia@gmail.com>
2022-10-18 20:03:57 -0400

    roll back for main the py
    back to square one (:

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+# from selenium import webdriver
+# import os
+
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument("--no-sandbox")
+# driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+# driver.get("https://google.com")
+
 from selenium import webdriver
 import os
+import sys
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-driver.get("https://google.com")
+driver = webdriver.Chrome("G:\My Drive\Programing\Personal scripts\Web\python selunium web bot\chromedriver.exe")
 
+input()
