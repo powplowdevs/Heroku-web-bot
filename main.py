c668437437103d6b26935194e9389e7011b8e9fd
powplowdevs <kalidfawzia@gmail.com>
2022-10-19 19:00:25 -0400

    giving up on selenium
    using webbrowser now

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-# from selenium import webdriver
-# import os
+import webbrowser
 
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-# chrome_options.add_argument("--headless")
-# chrome_options.add_argument("--disable-dev-shm-usage")
-# chrome_options.add_argument("--no-sandbox")
-# driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-# driver.get("https://google.com")
+webbrowser.open('https://google.com') 
 
-print("work???????????")
+input()
