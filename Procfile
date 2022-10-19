d781d16ecf4b95c1b105707b52989e194077f672
powplowdevs <kalidfawzia@gmail.com>
2022-10-18 20:00:29 -0400

    update to procfile and main.py
    still trying to get it to work with heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn main:app --log-file -
+web: python main.py
--- main.py
+++ main.py
@@ -6,7 +6,6 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get(f"D:\GIT HUB REPOS\Heroku-web-bot\chromedriver.exe"), chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 driver.get("https://google.com")
 
-input()
