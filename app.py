05a4c8939ae6aff42104a9b8025d410a48cc95e4
powplowdevs <kalidfawzia@gmail.com>
2022-10-20 18:16:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -33,33 +33,23 @@ def home():
 @app.route('/https/<url>')
 def root(url):    
     url = 'https://' + url
+    print(url)
     r = requests.get(url)
     rr = Response(response=r.content, status=r.status_code)
     rr.headers["Content-Type"] = r.headers['Content-Type']
     return rr
 
 @app.route('/g/<keyword>')
-def gkeyword(keyword):    
-    url = 'https://www.google.com/search'
+def gkeyword(keyword):     
+    url = 'https://www.google.com/search?q='
+    print(url)
     payload = {'q':keyword, 'num':1, 'start':1, 'sourceid':'chrome', 'ie':'UTF-8', 'cr':'cr=countryUS'}
     r = requests.get(url, params=payload)
     rr = Response(response=r.content, status=r.status_code)
     rr.headers["Content-Type"] = r.headers['Content-Type']
     return rr
 
-@app.route('/r/<subreddit>/subscribers')
-def gsubreddit(subreddit):
-    url = 'https://old.reddit.com/r/' + subreddit
-    xpath ="//span[@class='subscribers']/span[@class='number']/text()"
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    r = requests.get(url, headers=headers)
-    tree = html.fromstring(r.content)
-    subscribers = tree.xpath(xpath)
-    rr = Response(response=subscribers, status=r.status_code)
-    rr.headers["Content-Type"] = r.headers['Content-Type']
-    return rr
-
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 8000.
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port)
+    #port = int(os.environ.get('PORT', 8000))
+    app.run()
