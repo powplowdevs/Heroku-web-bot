f34746d660ee40227e163b4c4244740fbf069beb
Ayoub Mohamed <74332267+powplowdevs@users.noreply.github.com>
2022-10-20 12:08:04 -0400

    trying out flash proxy

--- app.py
+++ app.py
@@ -1,14 +1,65 @@
-from flask import Flask, render_template, request, flash
+# from flask import Flask, render_template, request, flash
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+
+# # main driver function
+# if __name__ == '__main__':
+ 
+#     # run() method of Flask class runs the application
+#     # on the local development server.
+#     app.run()
+
+import os
+import requests
+from lxml import html
+
+from flask import request
+from flask import Flask
+from flask import Response
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
 
-# main driver function
+@app.route('/')
+def home():
+    usage = 'Pass a properly encoded url parameter e.g. /https/www.google.com'
+    return usage
+
+@app.route('/https/<url>')
+def root(url):    
+    url = 'https://' + url
+    r = requests.get(url)
+    rr = Response(response=r.content, status=r.status_code)
+    rr.headers["Content-Type"] = r.headers['Content-Type']
+    return rr
+
+@app.route('/g/<keyword>')
+def gkeyword(keyword):    
+    url = 'https://www.google.com/search'
+    payload = {'q':keyword, 'num':1, 'start':1, 'sourceid':'chrome', 'ie':'UTF-8', 'cr':'cr=countryUS'}
+    r = requests.get(url, params=payload)
+    rr = Response(response=r.content, status=r.status_code)
+    rr.headers["Content-Type"] = r.headers['Content-Type']
+    return rr
+
+@app.route('/r/<subreddit>/subscribers')
+def gsubreddit(subreddit):
+    url = 'https://old.reddit.com/r/' + subreddit
+    xpath ="//span[@class='subscribers']/span[@class='number']/text()"
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    r = requests.get(url, headers=headers)
+    tree = html.fromstring(r.content)
+    subscribers = tree.xpath(xpath)
+    rr = Response(response=subscribers, status=r.status_code)
+    rr.headers["Content-Type"] = r.headers['Content-Type']
+    return rr
+
 if __name__ == '__main__':
- 
-    # run() method of Flask class runs the application
-    # on the local development server.
-    app.run()
+    # Bind to PORT if defined, otherwise default to 8000.
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
