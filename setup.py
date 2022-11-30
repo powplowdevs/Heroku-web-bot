78af8ea2d19b0c2966a750fd9f7263b0f1909702
Ayoub Mohamed <74332267+powplowdevs@users.noreply.github.com>
2022-11-30 12:28:10 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='Flask platform.sh',
+      version='1.0',
+      description='Flask platform.sh example',
+      author='Tim Toulas',
+      author_email='tim.toulas@...',
+      url='https://.../',
+      packages=find_packages(),
+     )
