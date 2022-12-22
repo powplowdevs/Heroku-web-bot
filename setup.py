fb501e20e1fadae6d50b31b74407c5b6bffa2f18
Ayoub Mohamed <74332267+powplowdevs@users.noreply.github.com>
2022-12-21 19:40:17 -0500

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='pyproprox.sh',
-      version='1.0',
-      description='pyprox',
-      author='Powplowdevs',
-      packages=find_packages(),
-     )
